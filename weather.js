2eb4d3b1b60d9ae6df02cd0baf9378731464fbbf
berkay oren <benberkayoren@gmail.com>
2022-09-28 21:06:12 +0300

    lesson

--- weather.js
+++ weather.js
@@ -3,14 +3,26 @@ const input = document.querySelector(".container input");
 const msg = document.querySelector("span.msg");
 const list = document.querySelector(".ajax-section .cities");
 
-localStorage.setItem("tokenKey", "Qz4DOSmGIic4qC2ylwIAmyS1TYddM0v0oDuClt");
-// localStorage.setItem(
-//   "tokenKeyEncripted",
-//   EncryptStringAES("4fe421f798cb1c4f14dd3423ae2428f6")
-// );
+localStorage.setItem(
+  "tokenKey",
+  "Qz4DOSmGIic4qC2ylwIAmyS1TYddM0v0oDuClt/rHpqa7Cypfm9/HMrnFEFjXMdk"
+);
 
-form.addEventListener("submit", () => {
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
   getWeatherDataFromApi();
 });
-//* get api func.
-cons;
+
+//Get api func. (http methods == Verbs)
+const getWeatherDataFromApi = async () => {
+  //alert("http request is gone!");
+  const tokenKey = DecryptStringAES(localStorage.getItem("tokenKey"));
+  //alert(tokenKey);
+  const inputValue = input.value;
+  const units = "metric";
+  const lang = "tr";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${tokenKey}&units=${units}&lang=${lang}`;
+
+  const response = await fetch(url).then((response) => response.json());
+  console.log(response);
+};
