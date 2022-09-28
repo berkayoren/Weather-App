2e4b1ae12deddee072cbb7b7f5ec7f4b8a261548
berkay oren <benberkayoren@gmail.com>
2022-09-28 22:00:04 +0300

    commit

--- weather.js
+++ weather.js
@@ -25,4 +25,38 @@ const getWeatherDataFromApi = async () => {
 
   const response = await fetch(url).then((response) => response.json());
   console.log(response);
+  const { main, sys, weather, name } = response;
+  const iconUrl = `http://openweathermap.org/img/wn/${weather[0].icon}@2x.png`;
+  const iconUrlAWS = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0].icon}.svg`;
+
+  const cityNameSpans = list.querySelectorAll(".card span");
+  const cityNameSpansArray = Array.from(cityNameSpans);
+  if (cityNameSpansArray.length > 0) {
+    const filteredArray = cityNameSpansArray.filter(
+      (span) => span.innerText == name
+    );
+    if (filteredArray.length > 0) {
+      msg.innerText = `You already know the weather for ${name}, Please search for another city.`;
+      form.reset();
+      return;
+    }
+  }
+  console.log(cityNameSpans);
+  const createdLi = document.createElement("li");
+  createdLi.classList.add("city");
+  createdLi.innerHTML = `<h2 class="city-name" data-name="${name}, ${
+    sys.country
+  }">
+                <span>${name}</span>
+                <sup>${sys.country}</sup>
+            </h2>
+            <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
+            <figure>
+                <img class="city-icon" src="${iconUrl}">
+                <figcaption>${weather[0].description}</figcaption>
+            </figure>`;
+
+  //*append vs prepend
+  list.prepend(createdLi);
+  form.reset(); // searchden sonra input sıfırlama
 };
